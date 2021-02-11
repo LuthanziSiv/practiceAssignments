23d7e2d5d2de581d86a0359fbc2c3ea4043eab0f
LuthanziSiv <luthanzisiv@gmail.com>
2021-02-11 10:13:13 +0200

    assignment
    Co-Authored-By: LuthanziSiv <78423754+LuthanziSiv@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: '',
+            item: [],
+            btn:'submit',
+            updateId:-1
+        }
+    }
+
+    
+    deleteItem = (val) => {
+        this.setState({
+            item: this.state.item.filter((it) => {
+                return it !== val
+            }),
+            todo:''
+        })
+    }
+    changeItem =(val,index)=>
+    {
+        this.setState({
+            todo:val,
+            btn:'update',
+            updateId:index
+        })
+        this.nameInput.focus()
+    }
+    handleClick = (e) => {
+        e.preventDefault()
+        if (this.state.updateId>=0)
+        {
+            this.state.item[this.state.updateId]=this.state.todo;
+            this.setState({
+                updateId:-1,
+                todo:'',
+                btn:'submit'
+            })
+        }
+       else
+        {
+            this.setState(prevState => (
+                {
+                    item: [...prevState.item, this.state.todo],
+                    todo: ''
+                }
+            ))
+            return this.state.item;
+        }
+    }
+
+    render() {
+        return (
+            <div className={"App"}>
+              <h1 className={"text"}>TO_DO LIST APP</h1>
+                <form onSubmit={this.handleClick}>
+                    <input type="text" value={this.state.todo} autoFocus
+                           ref={(input) => { this.nameInput = input; }}
+                           onChange={(e) => this.setState({todo: e.target.value})}/>
+                    <button type={"submit"}>{this.state.btn}</button>
+                    {
+                        this.state.item.map((it, index) => {
+                            if (it !== "") {
+                                return <div key={index}>
+                                    <p>{it}
+                                        <button type={"button"} style={{marginLeft: '50px'}} onClick={() => this.deleteItem(it)}>X
+                                        </button>
+                                        <button type={"button"} style={{marginLeft: '30px'}}
+                                                onClick={() => this.changeItem(it, index)}>edit
+                                        </button>
+                                    </p>
+                                </div>
+                            }
+                        })
+                    }
+                </form>
+            </div>
+        );
+    }
 }
 
 export default App;
